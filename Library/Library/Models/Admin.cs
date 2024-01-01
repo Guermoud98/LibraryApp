d97c0f6f922adf7edafde9f146a616dc0f1e79f0
Gur <guermoud.@gmail.com>
2024-01-01 23:58:59 +0100

    Added fields to the 'Employe' class

--- Library/Library/Models/Admin.cs
+++ Library/Library/Models/Admin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,28 +12,29 @@ namespace Library.Models
     {
         // Clé primaire de la table Admin
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //auto increment
         public int Id { get; set; }
 
         // Nom de l'administrateur (obligatoire avec un message d'erreur personnalisé si manquant)
         [Required(ErrorMessage = "Le champ Nom est requis.")]
-        public string Nom { get; set; }
+        public string nom { get; set; }
 
         // Prénom de l'administrateur (obligatoire avec un message d'erreur personnalisé si manquant)
         [Required(ErrorMessage = "Le champ Prénom est requis.")]
-        public string Prenom { get; set; }
+        public string prenom { get; set; }
 
         // Adresse e-mail de l'administrateur (obligatoire avec un message d'erreur personnalisé si manquant et validation du format de l'e-mail)
         [Required(ErrorMessage = "Le champ Adresse e-mail est requis.")]
         [EmailAddress(ErrorMessage = "Veuillez spécifier une adresse e-mail valide.")]
-        public string AdresseEmail { get; set; }
+        public string adresseEmail { get; set; }
 
         // Identifiant de l'administrateur (obligatoire avec un message d'erreur personnalisé si manquant)
         [Required(ErrorMessage = "Le champ Identifiant est requis.")]
-        public string Identifiant { get; set; }
+        public string identifiant { get; set; }
 
         // Mot de passe de l'administrateur (obligatoire avec un message d'erreur personnalisé si manquant)
         [Required(ErrorMessage = "Le champ Mot de passe est requis.")]
-        public string MotDePasse { get; set; }
+        public string motDePasse { get; set; }
 
     }
 }
--- Library/Library/Models/Employe.cs
+++ Library/Library/Models/Employe.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,34 @@ namespace Library.Models
 {
     internal class Employe
     {
+        // Clé primaire de la table Employe
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //auto increment
+        public int Id { get; set; }
+
+        // Nom de l'employé (obligatoire avec un message d'erreur personnalisé si manquant)
+        [Required(ErrorMessage = "Le champ Nom est requis.")]
+        public string nom { get; set; }
+
+        // Prénom de l'employé (obligatoire avec un message d'erreur personnalisé si manquant)
+        [Required(ErrorMessage = "Le champ Prénom est requis.")]
+        public string prenom { get; set; }
+
+        // Adresse e-mail de l'employé (obligatoire avec un message d'erreur personnalisé si manquant et validation du format de l'e-mail)
+        [Required(ErrorMessage = "Le champ Adresse e-mail est requis.")]
+        [EmailAddress(ErrorMessage = "Veuillez spécifier une adresse e-mail valide.")]
+        public string adresseEmail { get; set; }
+
+        // Identifiant de l'employé (obligatoire avec un message d'erreur personnalisé si manquant)
+        [Required(ErrorMessage = "Le champ Identifiant est requis.")]
+        public string identifiant { get; set; }
+
+        // Mot de passe de l'employé (obligatoire avec un message d'erreur personnalisé si manquant)
+        [Required(ErrorMessage = "Le champ Mot de passe est requis.")]
+        public string motDePasse { get; set; }
+
+        // Relation avec la table Reservation (un employé peut avoir plusieurs réservations)
+        public ICollection<Reservation> Reservations { get; set; }
+
     }
 }
