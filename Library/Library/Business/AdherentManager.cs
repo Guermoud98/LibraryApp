9b27a34f4b12e9a7152884850e6d076f59a77474
Gur <guermoud.@gmail.com>
2024-01-04 21:16:21 +0100

    Added the 'UpdateAdherent' method in the 'AdherentManager' inside the 'Business' folder.

--- Library/Library/Business/AdherentManager.cs
+++ Library/Library/Business/AdherentManager.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿using Library.DAO;
+using Library.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Library.Business
 {
     internal class AdherentManager
     {
+        private readonly AdherentDAO _adherentDAO;
+        public AdherentManager(AdherentDAO adherent)
+        {
+            _adherentDAO = adherent;
+        }
+        public void AddAdherent(Adherent adherent)
+        {
+            _adherentDAO.AddAdherent(adherent);
+        }
+        public void GetAdherentByID(int id)
+        {
+            _adherentDAO.GetAdherentByID(id);
+        }
+        public void UpdateAdherent(Adherent adherent)
+        {
+            _adherentDAO.UpdateAdherent(adherent);
+        }
+
 
     }
 }
