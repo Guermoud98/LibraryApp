34065fe49cf0c427078ecc54c643247f808c6c2c
Gur <guermoud.@gmail.com>
2024-01-04 22:05:55 +0100

    Added the 'GetLiveByID' method in the 'LivreManager' inside the 'Business' folder.

--- Library/Library/Business/LivreManager.cs
+++ Library/Library/Business/LivreManager.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library.DAO;
+using Library.Models;
 
 namespace Library.Business
 {
     internal class LivreManager
     {
+        private readonly LivreDAO _livreDAO;
+        public LivreManager(LivreDAO livreDAO)
+        {
+            _livreDAO = livreDAO;
+        }
+        public void AddLivre(Livre livre)
+        {
+            _livreDAO.AddLivre(livre);
+        }
+        public Livre GetLivreByID(int id)
+        {
+           return  _livreDAO.GetLivreByID(id);
+        }
+
     }
 }
