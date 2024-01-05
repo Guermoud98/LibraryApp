590d505caf7137bceac0319a4f1599dc0238d944
Gur <guermoud.@gmail.com>
2024-01-05 01:08:14 +0100

    Added EmployeManager class to the Business folder

---
+++ Library/Library/Business/EmployeManager.cs
@@ -0,0 +1,50 @@
+﻿using Library.DAO;
+using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Business
+{
+    internal class EmployeManager
+    {
+        private readonly EmployeDAO _employeDAO;
+        private readonly LivreDAO _livreDAO;
+        private readonly ReservationDAO _reservationDAO;
+
+        public EmployeManager(EmployeDAO employeDAO, LivreDAO livreDAO, ReservationDAO reservationDAO)
+        {
+            _employeDAO = employeDAO;
+            _livreDAO = livreDAO;
+            _reservationDAO = reservationDAO;
+        }
+
+        // Connexion d'un employé
+        public Employe Connecter(string identifiant, string motDePasse)
+        {
+            return _employeDAO.GetEmployeByIdentifiantPassword(identifiant, motDePasse);
+        }
+
+
+        // Création d'un nouveau livre
+        public void AddLivre(Livre livre)
+        {
+            _livreDAO.AddLivre(livre);
+        }
+
+        // Suppression d'un livre
+        public void RemoveLivre(int livreId)
+        {
+            _livreDAO.RemoveLivre(livreId);
+        }
+        // Consultation de tous les livres
+        public List<Livre> ConsulterLivres()
+        {
+            // Logique de consultation des livres
+            return _livreDAO.GetLivres();
+        }
+
+    }
+}
